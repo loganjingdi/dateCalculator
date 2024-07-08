780d0859dc956d0e12ccb85b39e17164ae7849b5
jdliu <jdliu>
2024-07-08 23:47:58 +0800

    feat:添加复利计算器

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import CompoundInterestCalculator from './components/CompoundInterestCalculator'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <CompoundInterestCalculator />
+  </React.StrictMode>
 )
